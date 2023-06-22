1fe0cb58a096260f4da3993e5c7f8f1c93fa14cc
Ubaidullah <researchfigma@gmail.com>
2023-06-22 15:43:58 +0500

    add footer in footer component

---
+++ src/App/Component/footer/Footer.js
@@ -0,0 +1,135 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    // <!-- footer section -->
+    // <!-- footer section -->
+
+    <footer class="footer">
+
+        <div class="footer__logo">
+
+            <img src="img/logo.png" alt="DZ food Logo">
+
+            <p class="paragraph">
+                Our job is to filling your tummy with
+                delicious food and with fast and free
+                delivery.
+            </p>
+
+            <div class="footer__icons">
+
+                <a href="#">
+                    <i class='bx bxl-instagram-alt'></i>
+                </a>
+
+                <a href="#">
+                    <i class='bx bxl-facebook'></i>
+                </a>
+
+                <a href="#">
+                    <i class='bx bxl-twitter'></i>
+                </a>
+
+            </div>
+
+        </div>
+
+        <div class="footer__linksection">
+
+            <div class="footer__links">
+
+                <span class="footer__linkheading">
+                    About
+                </span>
+
+                <a class="footer__link" href="#">
+                    About Us
+                </a>
+
+                <a class="footer__link" href="#">
+                    Features
+                </a>
+
+                <a class="footer__link" href="#">
+                    Menu
+                </a>
+            </div>
+
+
+            <div class="footer__links">
+
+                <span class="footer__linkheading">
+                    Company
+                </span>
+
+                <a class="footer__link" href="#">
+                    Why DZ food?
+                </a>
+
+                <a class="footer__link" href="#">
+                    Partner With Us
+                </a>
+
+                <a class="footer__link" href="#">
+                    FAQ
+                </a>
+
+                <a class="footer__link" href="#">
+                    Blog
+                </a>
+            </div>
+
+
+            <div class="footer__links">
+
+                <span class="footer__linkheading">
+                    Support
+                </span>
+
+                <a class="footer__link" href="#">
+                    Account
+                </a>
+
+                <a class="footer__link" href="#">
+                    Support Center
+                </a>
+
+                <a class="footer__link" href="#">
+                    Feedback
+                </a>
+
+                <a class="footer__link" href="#">
+                    Contact Us
+                </a>
+
+                <a class="footer__link" href="#">
+                    Accessibilty
+                </a>
+
+            </div>
+
+
+            <div class="footer__links">
+
+                <span class="footer__linkheading">
+                    Get in Touch
+                </span>
+
+                <a class="footer__link" href="#">
+                    Question or feedback?
+                </a>
+
+                <a class="footer__link" href="#">
+                    We’d love to hear from you
+                </a>
+
+            </div>
+
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
