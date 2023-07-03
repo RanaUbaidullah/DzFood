8a0b78645f9b7a27c43a505a61196f496d9bea06
Ubaidullah <researchfigma@gmail.com>
2023-07-03 13:41:03 +0500

    pagination add in menu

--- src/App/pages/menu/Menu.js
+++ src/App/pages/menu/Menu.js
@@ -4,10 +4,10 @@ import { Config } from "../../constant/Index";
 
 function Menu() {
   const [show, setShow] = useState(false);
-  const [data, setData] = useState();
-  const [catadata, setCatadata] = useState();
-  const [aimg, setAimg] = useState();
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(8);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,19 +21,6 @@ function Menu() {
       }
     };
 
-    const fetchDatacata = async () => {
-      try {
-        const response = await fetch(Config.serverUrlCategories);
-        const jsonData = await response.json();
-        setCatadata(jsonData.data);
-        console.log(catadata);
-        console.log("catadata");
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchDatacata();
     fetchData();
   }, []);
 
@@ -46,8 +33,25 @@ function Menu() {
   }
 
   const handleImageLoad = (image) => {
-    setAimg(`https://danzee.fra1.digitaloceanspaces.com/dzfood/admin/images/products/large/${image}`);
+    if (image == null) {
+      return "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
+    } else {
+      return `https://danzee.fra1.digitaloceanspaces.com/dzfood/admin/images/products/large/${image}`;
+    }
   };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
   return (
     <>
@@ -92,41 +96,51 @@ function Menu() {
         </div>
 
         <div className="menu__cards">
-          {data &&
-            data.map((item) => (
-              <div key={item.id} className="card">
-                <div className="rating">
-                  <h4>{item?.stars}</h4>
-                  <i className="fa-solid fa-star" />
-                  <span>({item?.cooking_time}+)</span>
+        {currentItems.map((item) => (
+            <div key={item.id} className="card">
+              <div className="rating">
+                <h4>{item?.stars}</h4>
+                <i className="fa-solid fa-star" />
+                <span>({item?.cooking_time}+)</span>
+              </div>
+              <div className="heart">
+                <i className="fa-regular fa-heart" />
+              </div>
+              <img
+                onLoad={() => handleImageLoad(item.image)}
+                src={handleImageLoad(item.image)}
+                alt="product img"
+              />
+              <div className="menu__content">
+                <div className="title__price">
+                  <h3 className="card__title">{item?.title?.en}</h3>
+                  <span className="price">
+                    Rs {item?.price ? item?.price : 0}
+                  </span>
                 </div>
-                <div className="heart">
-                  <i className="fa-regular fa-heart" />
+                <div className="time">
+                  <i className="fa-solid fa-stopwatch" />
+                  <span>10-15 mins</span>
                 </div>
-                <img
-                  onLoad={() => handleImageLoad(item.image)}
-                  src={aimg || "img/picture-not-available.jpg"}
-                  alt="product img"
-                />
-                <div className="menu__content">
-                  <div className="title__price">
-                    <h3 className="card__title">{item?.title?.en}</h3>
-                    <span className="price">
-                      Rs {item?.price ? item?.price : 0}
-                    </span>
-                  </div>
-                  <div className="time">
-                    <i className="fa-solid fa-stopwatch" />
-                    <span>10-15 mins</span>
-                  </div>
-                  <div className="food__type">
-                    <span className="type">Burger</span>
-                    <span className="type">Chicken</span>
-                    <span className="type">Fast Food</span>
-                  </div>
+                <div className="food__type">
+                  <span className="type">Burger</span>
+                  <span className="type">Chicken</span>
+                  <span className="type">Fast Food</span>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
+        </div>
+        <div className="pagination">
+          {pageNumbers.map((number) => (
+            <span 
+              key={number}
+              className={`page-number ${currentPage === number ? "active__pagination" : ""}`}
+              onClick={() => paginate(number)}
+            >
+              {number}
+            </span>
+          ))}
         </div>
       </div>
     </>
